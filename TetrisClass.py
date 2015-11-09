e5027164e919c95ab565ecd3022abd6c1e511d92
Sergii Getmanchuk <gsforwork@gmail.com>
2015-11-09 21:03:33 +0200

    Class Pole finished, Class Figure is in progress

--- TetrisClass.py
+++ TetrisClass.py
@@ -38,21 +38,149 @@ class pole(object):
         return res[:-1]
         
     def clear_full_lines(self):
-        pass
+        full_line = [1 for col in range(self._w)]
+        empty_line = [0 for col in range(self._w)]
+        count = 0
+        if full_line not in self._raw_grid:
+            return count
+        else:
+            for line in self._raw_grid:
+                if line == full_line:
+                    count += 1
+                    self._raw_grid.remove(line)
+                    self._raw_grid.insert(0, empty_line)
+        return count
         
     def curr_fig(self):
-        pass
+        shape = init_figures[random.choice(init_figures.keys())]
+#        print  shape
+        fig = figure(shape)
+#        print  fig
+        rotate = random.randrange(4)
+        for i in xrange(rotate):
+            fig.rotate_r()
+         
+        if self.next_fig == None:
+            self.next_fig = fig
+            return self.curr_fig()
+        else:
+            current_fig = self.next_fig
+            self.next_fig = fig
+            return current_fig
             
             
     def start_pos(self, figure):
-        pass
+        row = 0
+        col = (self._w - len(figure.get_figure()[0]))/2
+        return (row, col)
     
     def can_move_invariant(self, figure, current_pos, direction):
-        pass
+        tested_shape = []
+        if direction == 'right':
+            step = (0, 1)
+            
+            if current_pos[1] + len(figure.get_figure()[0]) + 1 > self._w:
+                return False
+                
+            num_shape_cells = len(figure.get_figure())
+            first_line = len(figure.get_figure()[0]) - 1 #last figure's column
+                        
+            tested_rows = range(len(figure.get_figure()))
+            tested_cols = list(reversed(range(len(figure.get_figure()[0]))))
+
+            for col in tested_cols:
+                for row in tested_rows:
+                    if figure.get_figure()[row][col] == 1:
+                        if not tested_shape:
+                            tested_shape.append ((row, col))
+                        else:
+                            to_append = True
+                            for cell in tested_shape:
+                                if cell[0] == row:
+                                    to_append = False
+                            if to_append:
+                                tested_shape.append ((row, col))
+            
+        elif direction == 'left':
+            step = (0, -1)
+            
+            if current_pos[1] - 1 < 0:
+                return False            
+            
+            num_shape_cells = len(figure.get_figure())
+            first_line = 0 #first figure's column
+
+            tested_rows = range(len(figure.get_figure()))
+            tested_cols = range(len(figure.get_figure()[0]))
+
+            for col in tested_cols:
+                for row in tested_rows:
+                    if figure.get_figure()[row][col] == 1:
+                        if not tested_shape:
+                            tested_shape.append ((row, col))
+                        else:
+                            to_append = True
+                            for cell in tested_shape:
+                                if cell[0] == row:
+                                    to_append = False
+                            if to_append:
+                                tested_shape.append ((row, col))
+            
+        elif direction == 'down':
+            step = (1, 0)
+            
+            if current_pos[0] + len(figure.get_figure()) + 1 > self._h:
+                self.state = "stop"
+                return False            
+            
+            num_shape_cells = len(figure.get_figure()[0])
+            first_line = len(figure.get_figure()) - 1 #last figure's row
+            
+            tested_rows = list(reversed(range(len(figure.get_figure()))))
+#            print tested_rows
+            tested_cols = range(len(figure.get_figure()[0]))
+#            print tested_cols
+
+            for row in tested_rows:
+                for col in tested_cols:
+                    if figure.get_figure()[row][col] == 1:
+                        if not tested_shape:
+                            tested_shape.append ((row, col))
+                        else:
+                            to_append = True
+                            for cell in tested_shape:
+                                if cell[1] == col:
+                                    to_append = False
+                            if to_append:
+                                tested_shape.append ((row, col))
+#                        tested_cols.remove(col)
+        
+        print tested_shape
+        print num_shape_cells
+        
+        assert num_shape_cells == len(tested_shape)
+        
+        for cell in tested_shape:
+            tested_row = current_pos[0] + cell[0] + step[0]
+            tested_col = current_pos[1] + cell[1] + step[1]
+            if self._raw_grid[tested_row][tested_col] == 1:
+                if direction == 'down': self.state = "stop"
+                return False
+                
+        return True
 
         
 
         
 class figure(object):
-    pass
-
+    def __init__(self, matrix):
+        pass
+        
+    def get_figure(self):
+        pass
+        
+    def rotate_r(self):
+        pass
+ 
+    def rotate_l(self):
+        pass
