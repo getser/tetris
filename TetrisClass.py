ad7668de6b95cd5550348943a26c1971b023e376
Sergii Getmanchuk <getser79@gmail.com>
2016-04-15 20:29:40 +0300

    PEP8 follow

--- TetrisClass.py
+++ TetrisClass.py
@@ -1,19 +1,22 @@
 import random
 
-init_figures = {'L' : [[1,0],[1,0],[1,1]], 
-            'bL' :  [[0,1],[0,1],[1,1]],
-            'Z' : [[1,1,0], [0,1,0],[0,1,1]],
-            'bZ' : [[0,1,1], [0,1,0],[1,1,0]],
-            'T' : [[1,1,1], [0,1,0],[0,1,0]],
-            'l' : [[1],[1],[1],[1]],
-            'E' : [[1,0], [1,1],[1,0]],
-            'O' : [[1,1],[1,1]]}
-            
-direction_steps = {'right':(0,1), 
-                   'left':(0,-1),
-                   'down':(1,0)}
+init_figures = {'L': [[1, 0], [1, 0], [1, 1]],
+                'bL':  [[0, 1], [0, 1], [1, 1]],
+                'Z': [[1, 1, 0], [0, 1, 0], [0, 1, 1]],
+                'bZ': [[0, 1, 1], [0, 1, 0], [1, 1, 0]],
+                'T': [[1, 1, 1], [0, 1, 0], [0, 1, 0]],
+                'l': [[1], [1], [1], [1]],
+                'E': [[1, 0], [1, 1], [1, 0]],
+                '4': [[1, 0], [1, 1], [0, 1]],
+                'b4': [[0, 1], [1, 1], [1, 0]],
+                'O': [[1, 1], [1, 1]]}
 
-class pole(object):
+direction_steps = {'right': (0, 1),
+                   'left': (0, -1),
+                   'down': (1, 0)}
+
+
+class Pole(object):
     """
     Creates a grid "pole", where everything is running.
     Takes integers "height" and "width" as dimensions.
@@ -22,8 +25,8 @@ class pole(object):
     def __init__(self, height, width):
         self._h = height
         self._w = width
-        self._raw_grid = [[0 for col in range(self._w)] 
-                             for row in range(self._h)] 
+        self._raw_grid = [[0 for col in range(self._w)]
+                          for row in range(self._h)]
         self.next_fig = None
         self.current_fig = None
         self.state = "run"  # indicates if current figure can move down
@@ -34,25 +37,25 @@ class pole(object):
         Returns "raw" grid, which contains all elements, that became static.
         """
         return self._raw_grid
-        
+
     def get_copy_grid(self):
         """
         Returns a copy of "raw" grid for any use.
         """
         return self._raw_grid[:]
-        
+
     def get_height(self):
         """
         Returns height of grid for any use.
         """
         return self._h
-    
+
     def get_width(self):
         """
         Returns width of grid for any use.
         """
         return self._w
-    
+
     def __str__(self):
         """
         Returns a string representation of "raw" grid.
@@ -61,7 +64,7 @@ class pole(object):
         for s in self.get_raw_grid():
             res += (str(s) + '\n')
         return res[:-1]
-        
+
     def clear_full_lines(self):
         """
         Clears lines of grid, which contain only ones.
@@ -79,54 +82,56 @@ class pole(object):
                     self._raw_grid.remove(line)
                     self._raw_grid.insert(0, empty_line)
         return count
-        
+
     def curr_fig(self):
         """
         Creates new next and current figures.
         """
-#        print "Im HERE, curr_fig"
+        # print "Im HERE, curr_fig"
         shape = init_figures[random.choice(init_figures.keys())]
-        fig = figure(shape)
+        fig = Figure(shape)
         rotate = random.randrange(4)
         for i in xrange(rotate):
             fig.rotate_r()
-         
-        if self.next_fig == None:
+
+        if self.next_fig is None:
             self.next_fig = fig
-            return self.curr_fig() #recursively creates the very first current figure
+            # recursively creates the very first current figure:
+            return self.curr_fig()
         else:
             current_fig = self.next_fig
             self.next_fig = fig
             return current_fig
-            
-            
+
     def start_pos(self, figure):
-#        print "Start_pos running"
+        # print "Start_pos running"
         """
         Returns start_pos - tuple of row and col of grid.
-        'start_pos' is where leftmost upper corner (0.0) of current figure be placed at start.
+        'start_pos' is where leftmost upper corner (0.0) of
+        current figure be placed at start.
         """
         row = 0
         col = (self._w - len(figure.get_figure()[0]))/2
         return [row, col]
-    
+
     def can_move_invariant(self, figure, current_pos, direction):
         """
-        Determines if given figure being at given position is able to move one step
-        to given direction.
-        If direction == "down" and figure can't move - changes game state to "stop".
+        Determines if given figure being at given position is able
+        to move one step to given direction.
+        If direction == "down" and figure can't move - changes
+        game state to "stop".
         """
         tested_shape = []
         if direction == 'right':
             step = (0, 1)
-            
+
             if current_pos[1] + len(figure.get_figure()[0]) + 1 > self._w:
-#                print "Can't move out of grid."
+                # print "Can't move out of grid."
                 return False
-                
+
             num_shape_cells = len(figure.get_figure())
-            first_line = len(figure.get_figure()[0]) - 1 #last figure's column
-                        
+            first_line = len(figure.get_figure()[0]) - 1  # last figure's colum
+
             tested_rows = range(len(figure.get_figure()))
             tested_cols = list(reversed(range(len(figure.get_figure()[0]))))
 
@@ -134,24 +139,24 @@ class pole(object):
                 for row in tested_rows:
                     if figure.get_figure()[row][col] == 1:
                         if not tested_shape:
-                            tested_shape.append ((row, col))
+                            tested_shape.append((row, col))
                         else:
                             to_append = True
                             for cell in tested_shape:
                                 if cell[0] == row:
                                     to_append = False
                             if to_append:
-                                tested_shape.append ((row, col))
-            
+                                tested_shape.append((row, col))
+
         elif direction == 'left':
             step = (0, -1)
-            
+
             if current_pos[1] - 1 < 0:
-#                print "Can't move out of grid."
-                return False            
-            
+                # print "Can't move out of grid."
+                return False
+
             num_shape_cells = len(figure.get_figure())
-            first_line = 0 #first figure's column
+            first_line = 0  # first figure's column
 
             tested_rows = range(len(figure.get_figure()))
             tested_cols = range(len(figure.get_figure()[0]))
@@ -160,73 +165,75 @@ class pole(object):
                 for row in tested_rows:
                     if figure.get_figure()[row][col] == 1:
                         if not tested_shape:
-                            tested_shape.append ((row, col))
+                            tested_shape.append((row, col))
                         else:
                             to_append = True
                             for cell in tested_shape:
                                 if cell[0] == row:
                                     to_append = False
                             if to_append:
-                                tested_shape.append ((row, col))
-            
+                                tested_shape.append((row, col))
+
         elif direction == 'down':
             step = (1, 0)
-            
+
             if current_pos[0] + len(figure.get_figure()) + 1 > self._h:
                 self.state = "stop"
-#                print "It's bottom here :)"
-                return False            
-            
+                # print "It's bottom here :)"
+                return False
+
             num_shape_cells = len(figure.get_figure()[0])
-            first_line = len(figure.get_figure()) - 1 #last figure's row
-            
+            first_line = len(figure.get_figure()) - 1  # last figure's row
+
             tested_rows = list(reversed(range(len(figure.get_figure()))))
-#            print tested_rows
+            # print tested_rows
             tested_cols = range(len(figure.get_figure()[0]))
-#            print tested_cols
+            # print tested_cols
 
             for row in tested_rows:
                 for col in tested_cols:
                     if figure.get_figure()[row][col] == 1:
                         if not tested_shape:
-                            tested_shape.append ((row, col))
+                            tested_shape.append((row, col))
                         else:
                             to_append = True
                             for cell in tested_shape:
                                 if cell[1] == col:
                                     to_append = False
                             if to_append:
-                                tested_shape.append ((row, col))
-                                
-#        print tested_shape
-#        print num_shape_cells
-        assert num_shape_cells == len(tested_shape) 
-        
+                                tested_shape.append((row, col))
+
+        assert num_shape_cells == len(tested_shape)
+
         for cell in tested_shape:
             tested_row = current_pos[0] + cell[0] + step[0]
             tested_col = current_pos[1] + cell[1] + step[1]
             if self._raw_grid[tested_row][tested_col] == 1:
-                if direction == 'down': self.state = "stop"
-#                print "Stopped with not empty cell."
+                if direction == 'down':
+                    self.state = "stop"
+                # print "Stopped with not empty cell."
                 return False
-        
-        #print "Is able to move " + direction + "!"
+
+        # print "Is able to move " + direction + "!"
         return True
 
     def move_figure(self, direction):
         """
-        Tests if current figure is able to move to given direction. 
+        Tests if current figure is able to move to given direction.
         (Uses can_move_invariant.)
-        If is able to move - changes current position for a step to given direction.
+        If is able to move - changes current position for
+        a step to given direction.
         """
         if self.current_pos:
-            if self.can_move_invariant(self.current_fig, self.current_pos, direction):
+            if self.can_move_invariant(self.current_fig,
+                                       self.current_pos,
+                                       direction):
                 step = direction_steps[direction]
                 self.current_pos[0] += step[0]
                 self.current_pos[1] += step[1]
-#            else:
-#                print '"self.current_pos" is absend' 
-        
+            # else:
+            #     print '"self.current_pos" is absend'
+
     def insert_figure(self):
         """
         Inserts current figure to "raw" grid at current position.
@@ -238,11 +245,10 @@ class pole(object):
                         pos_row = row + self.current_pos[0]
                         pos_col = col + self.current_pos[1]
                         assert self._raw_grid[pos_row][pos_col] == 0
-                        #print "trying to insert at pos:", (pos_row, pos_col)
+                        # print "trying to insert at pos:", (pos_row, pos_col)
                         self._raw_grid[pos_row][pos_col] = 1
-        else:
-#            print 'no "self.current_pos"'
-            
+        # else:
+        #     print 'no "self.current_pos"'
 
     def run_down(self):
         """
@@ -251,12 +257,11 @@ class pole(object):
         while self.state == "run":
             self.move_figure('down')
 
-            
     def run(self):
         """
         Make things roll ones.
         """
-        print "Runing" 
+        # print "Runing"
         self.current_fig = self.curr_fig()
         self.current_pos = self.start_pos(self.current_fig)
         d_move = 'down'
@@ -265,24 +270,22 @@ class pole(object):
         else:
             self.insert_figure()
             self.state = "run"
-        
-        
 
-        
-class figure(object):
+
+class Figure(object):
     """
     Used to create figures for a game.
-    Takes a "matrix" of figure (list of lists with '0' and '1') as argument
+    Takes a "matrix" of figure (list of lists with '0' and '1') as argument.
     """
     def __init__(self, matrix):
         self._fig = matrix
-        
+
     def get_figure(self):
         """
         Returns a list representation figure (to make indexing, for example).
         """
-        return self._fig    
-        
+        return self._fig
+
     def __str__(self):
         """
         Returns a string representation of a figure.
@@ -291,7 +294,7 @@ class figure(object):
         for s in self.get_figure():
             res += (str(s) + '\n')
         return res[:-1]
-        
+
     def rotate_r(self):
         """
         Rotates a figure 90 degree to the right.
@@ -299,12 +302,12 @@ class figure(object):
         res_rows = len(self._fig[0])
         res_cols = len(self._fig)
         res = [[0 for col in range(res_cols)] for row in range(res_rows)]
-        
+
         for row in range(len(self._fig)):
             for col in range(len(self._fig[0])):
-                res[col][(len(res[0]) - 1) - row] = self._fig[row][col]       
+                res[col][(len(res[0]) - 1) - row] = self._fig[row][col]
         self._fig = res
-    
+
     def rotate_l(self):
         """
         Rotates a figure 90 degree to the left.
@@ -312,11 +315,8 @@ class figure(object):
         res_rows = len(self._fig[0])
         res_cols = len(self._fig)
         res = [[0 for col in range(res_cols)] for row in range(res_rows)]
-        
+
         for row in range(len(self._fig)):
             for col in range(len(self._fig[0])):
-                res[(len(res) - 1) - col][row] = self._fig[row][col]       
+                res[(len(res) - 1) - col][row] = self._fig[row][col]
         self._fig = res
-
-
-
--- TetrisGUI.py
+++ TetrisGUI.py
@@ -7,7 +7,7 @@ import TetrisClass as tcg
 field_height = 30
 field_width = 15
 
-BLOCK_elem_width = 20
+BLOCK_elem_width = 25
 
 score = 0
 
@@ -17,60 +17,69 @@ HEITH = field_height * BLOCK_elem_width
 
 str_wind_size = str(field_width * BLOCK_elem_width + 40) + \
                     'x' + str(field_height * BLOCK_elem_width + 80)
-                    
+
 root = tk.Tk()  # defines the main window, assigned variable name 'root'
 
 root.geometry(str_wind_size+"+600+200")
 
 # """
- # creates window 600WIDTH x 800HEITH pixels that is offset 230 pix in x and
- # 180 pix in y from upper left corner of YOUR window.  To see the benefit
- # of the optional '+230+180' code comment out root.geometry(...) above
- # and uncomment the next line and note the new window is too high and left
-  # """
+# creates window 600WIDTH x 800HEITH pixels that is offset 230 pix in x and
+# 180 pix in y from upper left corner of YOUR window.  To see the benefit
+# of the optional '+230+180' code comment out root.geometry(...) above
+# and uncomment the next line and note the new window is too high and left
+# """
 # root.geometry(str_wind_size)
 
 frame1 = tk.Frame(root)
 
-frame1.grid(row = 0, column = 0)
+frame1.grid(row=0, column=0)
 
-canvas = tk.Canvas(root, width = field_width*BLOCK_elem_width, \
-                   height = field_height*BLOCK_elem_width, bg = "#1C1C1C")
-canvas.grid(row = 2, column = 0)
+canvas = tk.Canvas(root,
+                   width=field_width * BLOCK_elem_width,
+                   height=field_height * BLOCK_elem_width, bg="#1C1C1C")
+canvas.grid(row=2, column=0)
 
 # #canvas.pack(fill='both', expand=True)
 
-label1 = tk.Label(frame1, text = "Tetris").grid(row=0,column=0, sticky="nw")
-label2 = tk.Label(frame1, text = "Your current score is: " + str(score)).grid(row=0,column=1, sticky="nw")
+label1 = tk.Label(frame1, text="Tetris").grid(row=0, column=0, sticky="nw")
+label2 = tk.Label(frame1, text="Your current score is: " + str(score)).grid(
+                                                                row=0,
+                                                                column=1,
+                                                                sticky="nw")
 
 FILL_NEXT_BLOCK_CIRCLES = "grey"   # color fill for next block circles
 FILL_BLOCK_CIRCLES = "green"   # color fill for current block circles
 FILL_POLE_CIRCLES = "blue"   # color fill for pole circles
 
+
 def leftKey(event):
-#    print "< key pressed"
+    # print "< key pressed"
     if field.current_pos:
         field.move_figure('left')
-    
+
+
 def rightKey(event):
-#    print "< key pressed"
+    # print "< key pressed"
     if field.current_pos:
         field.move_figure('right')
-    
+
+
 def upKey(event):
-#    print "^ key pressed"
+    # print "^ key pressed"
     if field.current_fig:
         field.current_fig.rotate_r()
-    
+
+
 def downKey(event):
-#    print "Down key pressed"    
+    # print "Down key pressed"
     if field.current_pos:
         field.run_down()
-        
+
+
 def spKey(event):
-#    print "_____ key pressed"
+    # print "_____ key pressed"
     if field.current_fig:
-        field.current_fig, field.next_fig = field.next_fig, field.current_fig 
+        field.current_fig, field.next_fig = field.next_fig, field.current_fig
 
 frame1.focus_set()
 
@@ -80,19 +89,21 @@ def paint_pole():
     for row in xrange(len(block)):
         for col in xrange(len(block[0])):
             paint_pos_row = row
-            paint_pos_col = col            
+            paint_pos_col = col
             if block[row][col] == 1:
-                ball = canvas.create_oval(((paint_pos_col) * BLOCK_elem_width),\
-                                             paint_pos_row * BLOCK_elem_width,\
-                                           ((paint_pos_col + 1) * BLOCK_elem_width),\
-                                            (paint_pos_row + 1) * BLOCK_elem_width,\
-                                             width = 2, fill = FILL_POLE_CIRCLES)
+                ball = canvas.create_oval(
+                    ((paint_pos_col) * BLOCK_elem_width),
+                    paint_pos_row * BLOCK_elem_width,
+                    ((paint_pos_col + 1) * BLOCK_elem_width),
+                    (paint_pos_row + 1) * BLOCK_elem_width,
+                    width=2, fill=FILL_POLE_CIRCLES)
             else:
-                ball = canvas.create_rectangle(((paint_pos_col) * BLOCK_elem_width),\
-                                             paint_pos_row * BLOCK_elem_width,\
-                                           ((paint_pos_col + 1) * BLOCK_elem_width),\
-                                            (paint_pos_row + 1) * BLOCK_elem_width,\
-                                             width = 2, fill = "#1C1C1C")
+                ball = canvas.create_rectangle(
+                    ((paint_pos_col) * BLOCK_elem_width),
+                    paint_pos_row * BLOCK_elem_width,
+                    ((paint_pos_col + 1) * BLOCK_elem_width),
+                    (paint_pos_row + 1) * BLOCK_elem_width,
+                    width=2, fill="#1C1C1C")
 
 
 def paint_curr_figure():
@@ -103,12 +114,14 @@ def paint_curr_figure():
                 if block[row][col] == 1:
                     paint_pos_row = field.current_pos[0] + row
                     paint_pos_col = field.current_pos[1] + col
-                    ball = canvas.create_oval( ((paint_pos_col) * BLOCK_elem_width), \
-                                                paint_pos_row * BLOCK_elem_width, \
-                                                ((paint_pos_col + 1) * BLOCK_elem_width), \
-                                                (paint_pos_row + 1) * BLOCK_elem_width, \
-                                                width = 2, fill = FILL_BLOCK_CIRCLES)
-                                               
+                    ball = canvas.create_oval(
+                        ((paint_pos_col) * BLOCK_elem_width),
+                        paint_pos_row * BLOCK_elem_width,
+                        ((paint_pos_col + 1) * BLOCK_elem_width),
+                        (paint_pos_row + 1) * BLOCK_elem_width,
+                        width=2, fill=FILL_BLOCK_CIRCLES)
+
+
 def paint_next_figure():
     if field.next_fig:
         block = field.next_fig.get_figure()
@@ -117,53 +130,60 @@ def paint_next_figure():
                 if block[row][col] == 1:
                     paint_pos_row = row
                     paint_pos_col = field_width + col
-                    ball = canvas.create_oval( ((paint_pos_col) * BLOCK_elem_width * 0.7), \
-                                                paint_pos_row * BLOCK_elem_width * 0.7, \
-                                                ((paint_pos_col + 1) * BLOCK_elem_width * 0.7), \
-                                                (paint_pos_row + 1) * BLOCK_elem_width * 0.7, \
-                                                width = 2, fill = FILL_NEXT_BLOCK_CIRCLES)                                                
+                    ball = canvas.create_oval(
+                        ((paint_pos_col) * BLOCK_elem_width * 0.7),
+                        paint_pos_row * BLOCK_elem_width * 0.7,
+                        ((paint_pos_col + 1) * BLOCK_elem_width * 0.7),
+                        (paint_pos_row + 1) * BLOCK_elem_width * 0.7,
+                        width=2, fill=FILL_NEXT_BLOCK_CIRCLES)
+
 
 def game_play():
     global score
 
-    global field                                            
-    field = tcg.pole(field_height, field_width)
-    
+    global field
+    field = tcg.Pole(field_height, field_width)
+
     while True:
-         
         field.current_fig = field.curr_fig()
         field.current_pos = field.start_pos(field.current_fig)
         while field.state == "run":
             field.move_figure('down')
-                        
+
             frame1.bind('<Left>', leftKey)
             frame1.bind('<Right>', rightKey)
             frame1.bind('<Up>', upKey)
             frame1.bind('<Down>', downKey)
             frame1.bind('<space>', spKey)
-            
+
             paint_pole()
             paint_curr_figure()
             paint_next_figure()
             time.sleep(0.1)
-            if field.state != "run": 
+            if field.state != "run":
                 continue
             canvas.update()
-            
+
         else:
             field.insert_figure()
             curr_count = field.clear_full_lines()
             score += curr_count * 20
             field.state = "run"
             canvas.update()
-        
-        label2 = tk.Label(frame1, text = "Your current score is: " + str(score)).grid(row=0,column=1, sticky="nw")
+
+        label2 = tk.Label(frame1,
+                          text="Your current score is: " + str(score)).grid(
+                                                                row=0,
+                                                                column=1,
+                                                                sticky="nw")
 #        time.sleep(0.01)
 #        canvas.update()
 
-canvas.update()        
-Button1 = tk.Button(frame1,text = "New game", command = game_play).grid(row = 1,column = 0, sticky = "we")
-#Button1.pack()        
-               
-root.mainloop()
+canvas.update()
+Button1 = tk.Button(frame1, text="New game", command=game_play).grid(
+                                                                row=1,
+                                                                column=0,
+                                                                sticky="we")
+# Button1.pack()
 
+root.mainloop()
